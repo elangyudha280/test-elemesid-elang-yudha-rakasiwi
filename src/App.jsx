9e1462425421c91d27b17e1d842bae76cfcdb7be
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-07 12:45:39 +0700

    selesai buat component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
 
+// import component Navbar
+import Navbar from "./component/Navbar"
+
+
 // component App
 function App() {
 
   return (
     <div className="App">
-        
+        <Navbar/>
     </div>
   )
 }
