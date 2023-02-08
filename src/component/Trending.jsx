ce14bfd4281a3837ffcea3bd64c18e2b14d9f7b9
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-08 15:27:58 +0700

    selesai buat title trending

--- src/component/Trending.jsx
+++ src/component/Trending.jsx
@@ -3,7 +3,12 @@
 const Trending = ()=>{
     return (
         <section className="trending container">
-            
+            <h3 className="title-trending text-capitalize">
+                browser our trending
+            </h3>
+            <h4 className="sub-title-trending">
+                Receipt
+            </h4>
         </section>
     )
 }
