188841c1c62bf2dfad712d6f339c3094b14cc2ce
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-08 21:48:11 +0700

    mulai buat component footer mobile

--- src/component/footerCopyRight.jsx
+++ src/component/footerCopyRight.jsx
@@ -5,7 +5,7 @@
 
 const FooterCopyRight = ()=>{
     return (
-        <footer className="copy-right ">
+        <footer className="copy-right">
                 <span>© 2021 Elemes id. All rights reserved</span>
         </footer>
     )
